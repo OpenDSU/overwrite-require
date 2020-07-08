f3257a751f93f60454bf12e58f56527605bae698
skutner <sava.dumitru.daniel@gmail.com>
2020-07-08 16:07:41 +0300

    an error is thrown when a module is not found

--- index.js
+++ index.js
@@ -161,6 +161,10 @@ function enableForEnvironment(envType){
             }
         }
 
+        if (!result) {
+            throw Error(`Failed to load module ${request}`);
+        }
+
         enableRequire(request);
         if (previousRequireChanged) {
             //console.log("End loading library for require", request, $$.__global.requireLibrariesNames[request]);
